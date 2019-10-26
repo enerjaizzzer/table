f71d8e799ed353fd2c58f8a5638697f959352b9b
enerjaizzzer <enerjaizzzer@gmail.com>
2019-10-26 12:51:35 +0300

    feat: add default structure

---
+++ table/src/components/Table/Table.jsx
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+
+import './style.less';
+
+class Table extends Component {
+  mock = {
+    column: [
+      'num',
+      'first name',
+      'last name',
+      'country',
+      'city',
+      'street',
+      'home',
+      'phone',
+    ],
+    row: [
+      {
+        id: '0001',
+        name: {
+          firstName: 'Anton',
+          lastName: 'Kirienko',
+        },
+        adress: {
+          country: 'Belarus',
+          city: 'Gomel',
+          street: 'Street1',
+          home: '123'
+        },
+        phone: '+3354835187'
+      },
+      {
+        id: '0002',
+        name: {
+          firstName: 'Aleksandr',
+          lastName: 'Liakhovets',
+        },
+        adress: {
+          country: 'Belarus',
+          city: 'Gomel',
+          street: 'Street4',
+          home: '43v'
+        },
+        phone: '+44 566 5654 5'
+      },
+      {
+        id: '0003',
+        name: {
+          firstName: 'Aleksandr',
+          lastName: 'Borisenko',
+        },
+        adress: {
+          country: 'USA',
+          city: 'New York',
+          street: 'Street99',
+          home: '4444'
+        },
+        phone: '+544 5456 46 4'
+      },
+      {
+        id: '0004',
+        name: {
+          firstName: 'Vitali',
+          lastName: 'Sheremetiev',
+        },
+        adress: {
+          country: 'Belarus',
+          city: 'Dobrush',
+          street: 'Street1',
+          home: '1'
+        },
+        phone: '+5 5 54 4 56 4'
+      },
+    ]
+  }
+
+  render() {
+    return (
+      <div className="table__container">
+        asdasd
+      </div>
+    )
+  }
+}
+
+export default Table;
